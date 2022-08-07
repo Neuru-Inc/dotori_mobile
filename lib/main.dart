6033c4fe0bcd33bb52684fd040af7f09e7588471
sjsjsj1246 <sjsjsj1246@gmail.com>
2022-08-07 23:43:07 +0900

    add config, dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,14 @@ import 'package:dotori_mobile/screens/home_screen.dart';
 import 'package:dotori_mobile/screens/art_detail_screen.dart';
 import 'package:dotori_mobile/const/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(MaterialApp(
     initialRoute: '/',
     routes: {
